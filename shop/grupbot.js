72aab28eb18ec5664b5d00c66b81f8866230833c
Violet Himatsuki <105595001+Violetchan01@users.noreply.github.com>
2022-05-15 21:50:27 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,14 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+1. *Kagumi Support*
+_https://chat.whatsapp.com/FtQkEuWCh9LB5bfxmnLTWY_
+2. *Kagumi Support 2*
+_https://chat.whatsapp.com/FtQkEuWCh9LB5bfxmnLTWY_
+3. *Kagumi Support 3*
+_https://chat.whatsapp.com/FtQkEuWCh9LB5bfxmnLTWY_
+4. *Kagumi Support 4* 
+_https://chat.whatsapp.com/FtQkEuWCh9LB5bfxmnLTWY_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
